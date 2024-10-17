652da2c17dc802b62ef61c96399e4e9b1c50389f
Pepega <alireza.aboutalebi24@gmail.com>
2024-10-17 21:39:40 +0330

    Update CardCollection.jsx

--- src/pages/CardCollection/CardCollection.jsx
+++ src/pages/CardCollection/CardCollection.jsx
@@ -1,9 +1,11 @@
+import ChampionCards from "../../components/ChampionCards/ChampionCards";
 import Navigation from "../../components/Navigation/Navigation";
 
 const CardCollection = () => {
   return (
     <>
       <Navigation/>
+      <ChampionCards/>
     </>
   );
 };
