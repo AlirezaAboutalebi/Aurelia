3421c12d5d6f5d08c0748527f75b6225ffda4ef7
Pepega <alireza.aboutalebi24@gmail.com>
2024-10-20 18:47:26 +0330

    single pack design

--- src/pages/SinglePackPage/SinglePackPage.jsx
+++ src/pages/SinglePackPage/SinglePackPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import packsData from '../../data/packsDATA.json';
+import React from "react";
+import { useParams } from "react-router-dom";
+import packsData from "../../data/packsDATA.json";
+import "./SinglePackPage.css"; // Importing the new CSS
+import Navigation from "../../components/Navigation/Navigation";
 
 const SinglePackPage = () => {
   const { packId } = useParams();
@@ -11,13 +13,43 @@ const SinglePackPage = () => {
   }
 
   return (
-    <div className="single-pack-page">
-      <h1>{pack.packTitle}</h1>
-      <img src={pack.image} alt={pack.packTitle} />
-      <p>{pack.packDescription1}</p>
-      <p>{pack.packDescription2}</p>
-      <p>{pack.packDescription3}</p>
+    <>
+    <div className="figure-overlay"></div>
+    <Navigation/>
+
+    <div className="single-pack-container">
+      <div className="single-pack-content">
+        <h1 className="pack-realm">{pack.realmName}</h1>
+        <h2 className="pack-title">{pack.packTitle}</h2>
+
+        <p className="pack-description">{pack.packDescription1}</p>
+        <p className="pack-description">{pack.packDescription2}</p>
+        <p className="pack-description">{pack.packDescription3}</p>
+        <button className="open-pack-button">Open Pack Now</button>
+      </div>
+      <img
+        src={pack.image}
+        alt={pack.packTitle}
+        className="single-pack-image"
+      />
+      {/* Dice or other decorative images if needed */}
+      <img
+        src="/decoration/dice1.webp"
+        alt="dice1"
+        className="SinglePack__dice SinglePack__dice--1"
+      />
+      <img
+        src="/decoration/dice2.webp"
+        alt="dice2"
+        className="SinglePack__dice SinglePack__dice--2"
+      />
+      <img
+        src="/decoration/dice3.webp"
+        alt="dice3"
+        className="SinglePack__dice SinglePack__dice--3"
+      />
     </div>
+    </>
   );
 };
 
